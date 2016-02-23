63e48d0a7d828f9376cc5d3d0bdf10e2632bd449
Manuel Schermuly <prinzeru@gmail.com>
2016-02-23 01:46:41 +0100

    initial upload

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	nano = require('gulp-cssnano'),
+	minifyCss = require('gulp-minify-css'),
+	rename = require('gulp-rename');
+
+var imagemin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant');
+
+/* Compress images */
+gulp.task('images', function() {
+	return gulp.src('./theme/development/images/*.+(jpg|jpeg|gif|png|svg)')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('./theme/production/images/'));
+});
+
+/* Compile sass and minify css */
+gulp.task('styles', function() {
+	/* Compile nested (adding @charset "utf-8") */
+	gulp.src('./theme/development/scss/*.scss')
+		.pipe(sass({outputStyle: 'nested'})
+			.on('error', sass.logError))
+		.pipe(gulp.dest('./theme/production/css/'))
+	;
+	/* Compile compressed (no added charset) */
+	gulp.src('/theme/development/scss/*.scss')
+		.pipe(sass({outputStyle: 'compressed'})
+			.on('error', sass.logError))
+		.pipe(rename({suffix: '.min'}))
+		//.pipe(nano({zindex:false}))
+		.pipe(minifyCss())
+		.pipe(gulp.dest('./theme/production/css/'))
+	;
+});
+
+/* Watch sass file changes */
+gulp.task('watch', function() {
+	gulp.watch('./theme/development/scss/*.scss', ['styles']);
+});
+
+gulp.task('default', ['styles', 'watch'], function() {
+
+});
